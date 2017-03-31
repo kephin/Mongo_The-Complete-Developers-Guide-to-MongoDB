6823d821bc545a9229a4f13c49b7cf50ac9053d5
kephin <wunshon76325@gmail.com>
2017-03-31 23:56:37 +0800

    add default promise

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', () => console.log('Here we go!'))
