c6ab3a3ed2b2397cbd577ee886ffe156f66e00cc
kephin <wunshon76325@gmail.com>
2017-03-31 23:55:30 +0800

    drop users collection before each test

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -4,3 +4,8 @@ mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', () => console.log('Here we go!'))
   .on('error', err => console.warn('Warning', err));
+
+beforeEach((done) => {
+  // drop accept a callback function
+  mongoose.connection.collections.users.drop(() => done());
+});
