ba140f96520668146b0aab2e5a5ec05a6373644e
kephin <wunshon76325@gmail.com>
2017-04-02 03:56:12 +0800

    test for handling failed insert

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -15,4 +15,15 @@ describe('Validating records', () => {
     const { message } = validateResult.errors.name;
     assert(message === 'Name must be longer than 2 characters.');
   });
+
+  // Handling failed inserts
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch(validationResult => {
+        const { message } = validationResult.errors.name;
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
+  });
 });
