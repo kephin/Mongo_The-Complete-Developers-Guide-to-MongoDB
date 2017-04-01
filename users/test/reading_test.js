b7606e4cd954494403e06b82954b3a4ec07c7f3a
kephin <wunshon76325@gmail.com>
2017-04-01 23:26:23 +0800

    using find() class method and check with _id object

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -8,4 +8,14 @@ describe('Readig records', () => {
     kevin = new User({ name: 'kevin' });
     kevin.save().then(() => done());
   });
+
+  it('finds all users with name of kevin', (done) => {
+    // User.find => returns an array
+    User.find({ name: 'kevin' })
+      .then(users => {
+        // _id is an object
+        assert(users[0]._id.toString() === kevin._id.toString());
+        done();
+      });
+  });
 });
