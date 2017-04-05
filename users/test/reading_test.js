3c920c8fe35784e33cc5dd51a4d290593bc01897
kephin <wunshon76325@gmail.com>
2017-04-06 01:32:56 +0800

    handling pagination using skip and limit

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Readig records', () => {
-  let kevin;
+  let kevin, alex, zach, stephen;
 
   beforeEach((done) => {
     kevin = new User({ name: 'kevin' });
-    kevin.save().then(() => done());
+    alex = new User({ name: 'alex' });
+    zach = new User({ name: 'zach' });
+    stephen = new User({ name: 'stephen' });
+    Promise.all([kevin.save(), alex.save(), zach.save(), stephen.save()])
+      .then(() => done());
   });
 
   it('finds all users with name of kevin', (done) => {
@@ -24,6 +28,19 @@ describe('Readig records', () => {
     User.findOne({ _id: kevin._id })
       .then(user => {
         assert(user.name = 'kevin');
+        done();
+      });
+  });
+
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 }) // 1 => accending, -1 => descending
+      .skip(1)
+      .limit(2)
+      .then(users => {
+        assert(users[0].name === 'kevin');
+        assert(users[1].name === 'stephen');
+        assert(users.length === 2);
         done();
       });
   });
