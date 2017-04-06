dffb417db47dc233342f4d05221ab8f0479fe510
kephin <wunshon76325@gmail.com>
2017-04-06 23:03:16 +0800

    add album schema

--- UpStarMusic/database/models/album.js
+++ UpStarMusic/database/models/album.js
@@ -1,1 +1,13 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const albumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  imgage: String,
+  revenue: Number,
+})
+
+module.exports = albumSchema;
