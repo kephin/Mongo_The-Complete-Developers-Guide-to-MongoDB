078fc424a22d8b85cdd5a2f96c193af5f335b3dd
kephin <wunshon76325@gmail.com>
2017-04-07 01:03:21 +0800

    add artist model

--- UpStarMusic/database/models/artist.js
+++ UpStarMusic/database/models/artist.js
@@ -1,1 +1,20 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const albumSchema = require('./album');
+
+const artistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  Albums: [albumSchema],
+})
+
+const Artist = mongoose.model('Artist', artistSchema);
+
+module.exports = Artist;
