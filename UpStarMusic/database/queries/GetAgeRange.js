975c2617b97f679dd10accde94e8cf60c8836c6e
kephin <wunshon76325@gmail.com>
2017-04-07 02:40:14 +0800

    add min and max queries

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  // Never try to pull all the records form MongoDB to the Node environment
+  // return Artist.find({})
+  //   .sort({ age: 1 })
+  //   .then(users => ({ min: users[0].age, max: users[users.length - 1].age }));
+
+  const minQuery = Artist.find({})
+    .sort({ age: 1 })
+    // Instead, use limit() to get the first one
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  const maxQuery = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  return Promise.all([minQuery, maxQuery])
+    .then(res => ({ min: res[0], max: res[1] }));
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minQuery = Artist.find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+
+  const maxQuery = Artist.find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+
+  return Promise.all([minQuery, maxQuery])
+    .then(res => ({ min: res[0], max: res[1] }));
 };
