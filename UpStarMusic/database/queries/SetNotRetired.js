d4251ae127233d0dcb9374415d37c9fa10d4b180
kephin <wunshon76325@gmail.com>
2017-04-11 01:17:44 +0800

    add retired queries

--- UpStarMusic/database/queries/SetNotRetired.js
+++ UpStarMusic/database/queries/SetNotRetired.js
@@ -6,4 +6,9 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves after the update
  */
 module.exports = (_ids) => {
+  return Artist.updateMany({
+    _id: { $in: _ids }
+  }, {
+    $set: { retired: false }
+  });
 };
